e12e780ffbf1a0b963f50717b141e7dab26e88d4
William Light <visinin@gmail.com>
2009-08-09 11:10:48 +0300

    Update copyright notice.

--- src/rove_pattern.c
+++ src/rove_pattern.c
@@ -1,6 +1,6 @@
 /**
  * This file is part of rove.
- * rove is copyright 2007, 2008 william light <visinin@gmail.com>
+ * rove is copyright 2007-2009 william light <visinin@gmail.com>
  *
  * rove is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
